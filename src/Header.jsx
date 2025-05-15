7a36a613e7c80583c9a693654b5157c1da445e64
David <idjavid@gmail.com>
2025-05-15 13:34:12 +0200

    Body deleted becouse of the new structure

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MainMenu from './MainMenu';
+
+function Header() {
+  return (
+    <header>
+      <Navbar />
+      <MainMenu />
+    </header>
+  );
+}
+
+export default Header;
